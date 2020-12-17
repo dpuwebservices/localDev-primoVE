c6883e49cbab80151c56942394d69f2163fa3102
Christine McClure <camcclure@gmail.com>
2020-12-17 15:04:13 -0600

    copy prod preprod to test changes

---
+++ primo-explore/custom/01CARLI_DPU-CARLI_PREPROD/js/main.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const libchat = require("./dpu_modules/libchat");
+libchat.init();
+
+const htApp = require("./dpu_modules/hathitrust");
+htApp.init();
+
+(function(){
+
+     
+    /****************************************************************************************************/
+  
+        /*In case of CENTRAL_PACKAGE - comment out the below line to replace the other module definition*/
+      // const app = angular.module("viewCustom", ["angularLoad"]);
+  
+    /****************************************************************************************************/
+    // Code from UIC to initially collapse I-Share libraries
+    app.component("prmAlmaOtherMembersAfter", {
+        bindings: {
+          parentCtrl: "<",
+        },
+        controller: [
+          function () {
+            var ctrl = this;
+            ctrl.parentCtrl.isCollapsed = true;
+          },
+        ],
+      });
+
+          
+  
+  })();
