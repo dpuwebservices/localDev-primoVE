0abbc1886275fb5d917a6194b59cee9beea30eb5
Christine McClure <camcclure@gmail.com>
2021-04-01 15:55:23 -0500

    stackmap code working. in production view, but commented out until ready to launch

--- primo-explore/custom/01CARLI_DPU-CARLI_DPU/js/main.js
+++ primo-explore/custom/01CARLI_DPU-CARLI_DPU/js/main.js
@@ -1,11 +1,13 @@
 "use strict";
 
 const libchat = require("./dpu_modules/libchat");
+const stackmap = require("./dpu_modules/stackmap");
 const appMods = require("./dpu_modules/app-mods");
 
 
 appMods.init();     
-libchat.init();     
+libchat.init();   
+// stackmap.init();  *** uncomment when ready to open stacks for browsing
 
 // (function(){
 
