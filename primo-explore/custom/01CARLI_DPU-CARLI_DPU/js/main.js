c0948ae7a56803a85eef6cdf5dbbed9fcc767214
Christine McClure <camcclure@gmail.com>
2022-01-14 15:53:38 -0600

    formatting comment

--- primo-explore/custom/01CARLI_DPU-CARLI_DPU/js/main.js
+++ primo-explore/custom/01CARLI_DPU-CARLI_DPU/js/main.js
@@ -1,7 +1,8 @@
 "use strict";
 
 const libchat = require("./dpu_modules/libchat");
-// const stackmap = require("./dpu_modules/stackmap"); *** uncomment when online maps are updated
+// const stackmap = require("./dpu_modules/stackmap"); 
+// *** uncomment when online maps are updated
 const appMods = require("./dpu_modules/app-mods");
 
 
